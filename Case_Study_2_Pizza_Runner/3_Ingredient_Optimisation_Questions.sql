0ec60b0240955c90203291dc003c0560c6698592
selimgokirmak <86617834+selimgokirmak@users.noreply.github.com>
2022-08-19 23:32:54 +0300

    Update 3_Ingredient_Optimisation_Questions.sql

--- Case_Study_2_Pizza_Runner/3_Ingredient_Optimisation_Questions.sql
+++ Case_Study_2_Pizza_Runner/3_Ingredient_Optimisation_Questions.sql
@@ -47,13 +47,18 @@ LIMIT 1
 */
 
 
--- 
+-- Generate an order item for each record in the customers_orders table in the format of one of the following:
+-- Meat Lovers
+-- Meat Lovers - Exclude Beef
+-- Meat Lovers - Extra Bacon
+-- Meat Lovers - Exclude Cheese, Bacon - Extra Mushroom, Peppers
 
 
+-- Generate an alphabetically ordered comma separated ingredient list for each pizza order from the customer_orders table and add a 2x in front of any relevant ingredients
+-- For example: "Meat Lovers: 2xBacon, Beef, ... , Salami"
 
 
-
-
+-- What is the total quantity of each ingredient used in all delivered pizzas sorted by most frequent first?
 
 
 
